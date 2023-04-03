b045f1d940b690c557bcfa65dafd6abfa4ba93bd
Vitalii <zabrodskyi.vitalii@gmail.com>
2023-04-03 17:04:01 +0300

    Add daily update, some updates

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ from tgbot.middlewares.scheduler import SchedulerMiddleware
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from tgbot.handlers.alert import alert_check
+from tgbot.handlers.daily import daily_news
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +40,7 @@ def register_all_handlers(dp):
 
 def set_scheduled_jobs(scheduler, bot):
     scheduler.add_job(alert_check, "interval", seconds=30, args=(bot, ))
+    scheduler.add_job(daily_news, "cron", hour=6, minute=0, second=0, args=(bot,))
 
 
 async def main():
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -2,8 +2,9 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 
 
-async def user_start(message: Message):
-    await message.reply("Hello, user!")
+async def user_start(message: Message):  # Temp command for test, i don't need it now
+    await message.reply(f"Привіт, {message.from_user.first_name}!\n\n"
+                        f"Я просто бот, нажаль, наразі я не виконую ніякої функції у цьому чаті.")
 
 
 def register_user(dp: Dispatcher):
