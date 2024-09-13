c0456df59814f084433f1b595aaac9ef16dc8636
Vitalii <zabrodskyi.vitalii@gmail.com>
2024-09-13 14:01:55 +0300

    Add new feature, minor fix

--- tgbot/functions/cctv.py
+++ tgbot/functions/cctv.py
@@ -1,4 +1,8 @@
+import asyncio
+
 import cv2
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 from tgbot.config import load_config
 
@@ -8,22 +12,81 @@ logger = logging.getLogger(__name__)
 
 rtsp_url = load_config(".env").tg_bot.rtsp_url
 
+executor = ThreadPoolExecutor()
 
-def capture_rtsp_screenshot(output_file="yard.png"):
-    # Open RTSP stream
-    cap = cv2.VideoCapture(rtsp_url)
 
-    if not cap.isOpened():
-        logger.warning(f"Error: Unable to open RTSP stream")
-        return
+def capture_rtsp_screenshot_sync(output_file="yard.png"):
+    try:
+        # Open RTSP stream
+        cap = cv2.VideoCapture(rtsp_url)
 
-    # Read a frame from the stream
-    ret, frame = cap.read()
+        if not cap.isOpened():
+            logging.warning("Unable to open RTSP stream.")
+            return
 
-    if ret:
-        # Save the frame as an image
-        cv2.imwrite(output_file, frame)
-        logger.info(f"Screenshot saved to {output_file}")
-    else:
-        logger.warning("Error: Unable to capture a frame from the RTSP stream")
-    cap.release()
+        # Optionally skip frames to quickly get the latest
+        for _ in range(5):  # Skip first 5 frames for speed, adjust as necessary
+            cap.grab()
+
+        # Read a frame from the stream
+        ret, frame = cap.read()
+
+        if ret:
+            # Save the frame as an image
+            cv2.imwrite(output_file, frame)
+            logging.info(f"Screenshot saved to {output_file}")
+        else:
+            logging.warning("Error: Unable to capture a frame from the RTSP stream")
+
+    except Exception as e:
+        logging.error(f"An exception occurred: {e}")
+    finally:
+        # Always release the capture object
+        cap.release()
+
+
+def capture_rtsp_video_sync(output_video="yard.mp4", video_duration=10, fps=20):
+    try:
+        # Open RTSP stream
+        cap = cv2.VideoCapture(rtsp_url)
+
+        if not cap.isOpened():
+            logging.warning("Unable to open RTSP stream")
+            return
+
+        # Get frame width and height
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        # Define the codec and create VideoWriter object for video recording
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output_video, fourcc, fps, (frame_width, frame_height))
+
+        # Record video for the specified duration
+        start_time = time.time()
+        while (time.time() - start_time) < video_duration:
+            ret, frame = cap.read()
+            if ret:
+                out.write(frame)  # Write the frame into the video file
+            else:
+                logging.warning("Error: Unable to capture a frame during video recording")
+                break
+
+        logging.info(f"Video saved to {output_video}")
+
+    except Exception as e:
+        logging.error(f"An exception occurred: {e}")
+    finally:
+        # Always release the capture object and video writer
+        cap.release()
+        out.release()
+
+
+async def capture_rtsp_screenshot(output_file="yard.png"):
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(executor, capture_rtsp_screenshot_sync, output_file)
+
+
+async def capture_rtsp_video(output_video="yard.mp4", video_duration=10, fps=20):
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(executor, capture_rtsp_video_sync, output_video)
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,16 +1,17 @@
 from aiogram import Dispatcher, Bot
 from aiogram.types import Message, InputFile, ChatActions
 from tgbot.handlers.daily import get_weather_data, get_daily_news
-from tgbot.functions.cctv import capture_rtsp_screenshot
+from tgbot.functions.cctv import capture_rtsp_screenshot, capture_rtsp_video
 from tgbot.functions.crypto import crypto, index_price
 from tgbot.functions.tuya_devices import tuya_sensors_info
+from tgbot.functions.fun import martingale
 
 import logging
 
 logger = logging.getLogger(__name__)
 
 import time
-from random import randint
+from random import randint, uniform
 import g4f
 
 
@@ -26,14 +27,28 @@ users_info = {}  # collection of users with total liquid accumulated
 
 async def get_yard_img(message: Message):
     try:
-        capture_rtsp_screenshot()
+        await capture_rtsp_screenshot()
         img = InputFile("yard.png")
         await message.answer_chat_action(action=ChatActions.UPLOAD_PHOTO)
         await message.reply_photo(photo=img)
-        logger.info(f"Cam1 info was sent to {message.from_user.full_name}/{message.from_user.id}")
+        logger.info(f"Cam1 photo was sent to {message.from_user.full_name}/{message.from_user.id}")
+        # os.remove("yard.png")
     except Exception as err:
         await message.answer(text=str(err))
-        logger.error(f"Problem with send RTSP capture: {err}")
+        logger.error(f"Problem with send RTSP capture: <code>{err}</code>")
+
+
+async def get_yard_vid(message: Message):
+    try:
+        await capture_rtsp_video()
+        mp4 = InputFile("yard.mp4")
+        await message.answer_chat_action(action=ChatActions.UPLOAD_VIDEO)
+        await message.reply_video(video=mp4)
+        logger.info(f"Cam1 video was sent to {message.from_user.full_name}/{message.from_user.id}")
+        # os.remove("yard.mp4")
+    except Exception as err:
+        await message.answer(text=str(err))
+        logger.error(f"Problem with send RTSP video: <code>{err}</code>")
 
 
 async def get_weather(message: Message):
@@ -87,6 +102,7 @@ async def clear_answers(message: Message):
     await message.delete()
 
 
+# This func should be deleted or changed to other in soon updates
 async def ask_gpt(message: Message):
     user_id = message.from_user.id
     user_input = message.reply_to_message.text
@@ -135,10 +151,38 @@ async def get_tuya_sensors_info(message: Message):
     logger.info(f"Tuya sensors info was sent to {message.from_user.full_name}/{message.from_user.id}")
 
 
+async def get_martingale(message: Message):
+    args = message.get_args().split()
+
+    if len(args) == 0:
+        rand_balance = randint(500, 100000)
+        rand_bet = randint(1, 500)
+        await message.reply(f"Ну да, {message.from_user.first_name}, треба ж потикати...\n"
+                             f"Ладно, тобі рандомний баланс <code>{rand_balance}</code> гривні та початкова ставка буде <code>{rand_bet}</code>.\nБудеш винен.")
+        await message.answer(martingale(rand_balance, rand_bet))
+
+    else:
+        try:
+            balance = float(args[0])
+            bet = float(args[1])
+            if balance <= 100000000 and bet <= 100000000:
+                await message.reply(martingale(balance, bet))
+            else:
+                xd = round(uniform(50, 6000), 2)
+                await message.answer(f"""💥 Frontier вийшов з ладу.\n@{message.from_user.username}, вам доведеться заплатити заборгованість за використання надлишкових екзафлопс, у розмірі <b>{xd}$</b>. Майте на увазі, що борг віднесено до вашого облікового запису, що має ідентифікатор <code>{message.from_user.id}</code>. Будь які спроби піти від відповідальності будуть переслідуватись згідно місцевого законодавства.""")
+        except TypeError:
+            await message.reply("Отримано невірний тип данних. Все просто ж.. Вкажи баланс та початкову ставку.\n<code>/martingale 1000 10</code>")
+        except IndexError:
+            await message.reply("Відсутня необхідна кількість аргументів. Все просто ж.. Вкажи баланс та початкову ставку.\n<code>/martingale 1000 10</code>")
+        except ValueError:
+            await message.reply("Невірне значення аргументів. Все просто ж.. Вкажи баланс та початкову ставку.\n<code>/martingale 1000 10</code>")
+
+
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
     dp.register_message_handler(get_weather, commands=["weather"])
     dp.register_message_handler(get_yard_img, commands=["cam1"])
+    dp.register_message_handler(get_yard_vid, commands=["cam1v"])
     dp.register_message_handler(cum_joke, commands=["cum"])
     dp.register_message_handler(news, commands=["news"])
     dp.register_message_handler(ask_gpt, commands=["ask"])
@@ -146,3 +190,4 @@ def register_user(dp: Dispatcher):
     dp.register_message_handler(get_crypto_price, commands=["price"])
     dp.register_message_handler(get_index_price, commands=["index"])
     dp.register_message_handler(get_tuya_sensors_info, commands=["sensors"])
+    dp.register_message_handler(get_martingale, commands=["martingale"])
